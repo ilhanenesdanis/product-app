2db5ca6d84dfda6c1edc17d37dd7082b91683875
ilhan enes daniş <danisenes3@gmail.com>
2024-10-01 22:32:17 +0300

    productrepository test

--- test/infrasracture/product_repository_test.go
+++ test/infrasracture/product_repository_test.go
@@ -1,19 +1,86 @@
 package infrasracture
 
 import (
+	"context"
+	"os"
+	"product-app/common/postgresql"
+	"product-app/domain"
+	"product-app/persistence"
 	"testing"
 
+	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAdd(t *testing.T) {
+var productRepository persistence.IProductRepository
+var dbPool *pgxpool.Pool
+var ctx context.Context
 
-	t.Run("Test Add", func(t *testing.T) {
-		actual := Add(10, 20)
-		assert.Equal(t, 30, actual)
+func TestMain(m *testing.M) {
+	ctx = context.Background()
+
+	dbPool = postgresql.GetConnectionPool(ctx, postgresql.Config{
+		Host:                  "localhost",
+		Port:                  "6432",
+		DbName:                "productapp",
+		UserName:              "postgres",
+		Password:              "postgres",
+		MaxConnections:        "10",
+		MaxConnectionIdleTime: "30s",
 	})
+	productRepository = persistence.NewProductRepository(dbPool)
+
+	exitCode := m.Run()
+
+	os.Exit(exitCode)
 }
 
-func Add(x int, y int) int {
-	return x + y
+func setup(ctx context.Context, dbpool *pgxpool.Pool) {
+	TestDataInitialize(ctx, dbpool)
+}
+func clear(ctx context.Context, dbpool *pgxpool.Pool) {
+	TruncateTestData(ctx, dbpool)
+}
+
+func TestGetAllProduct(t *testing.T) {
+	setup(ctx, dbPool)
+	expectedProducts := []domain.Product{
+		{
+			Id:       1,
+			Name:     "AirFryer",
+			Price:    3000.0,
+			Discount: 22.0,
+			Store:    "ABC TECH",
+		},
+		{
+			Id:       2,
+			Name:     "Ütü",
+			Price:    1500.0,
+			Discount: 10.0,
+			Store:    "ABC TECH",
+		},
+		{
+			Id:       3,
+			Name:     "Çamaşır Makinesi",
+			Price:    10000.0,
+			Discount: 15.0,
+			Store:    "ABC TECH",
+		},
+		{
+			Id:       4,
+			Name:     "Lambader",
+			Price:    2000.0,
+			Discount: 0.0,
+			Store:    "Dekorasyon Sarayı",
+		},
+	}
+	t.Run("GetAllProducts", func(t *testing.T) {
+		actualProducts := productRepository.GetAllProducts()
+
+		assert.Equal(t, 4, len(actualProducts))
+
+		assert.Equal(t, expectedProducts, actualProducts)
+	})
+
+	clear(ctx, dbPool)
 }
